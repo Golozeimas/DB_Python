5f0dc211ffcb3d6844be8f71111490c76d478bf0
Matheus+ <joaosobramatheus@gmail.com>
2026-01-15 12:51:02 -0300

    <study> Atualiza dados de forma mais concisa

--- 5-atualiza.py
+++ 5-atualiza.py
@@ -4,7 +4,11 @@ conexao = sqlite3.connect("entretenimento.db")
 
 cursor = conexao.cursor()
 
-cursor.execute("UPDATE filmes SET nome_filme='The batman' WHERE nome_filme='Coringa'")
+# vamos fazer diferente e utilizar o input pra atualizar os dados
+# você pode fazer utilizando a mesma função execute
+id = 2
+nome_do_novo_filme = input("Digite o nome do novo filme: \n")
+cursor.execute("UPDATE filmes SET nome_filme=? WHERE id=? ", (nome_do_novo_filme, id))
 
 conexao.commit()
 
